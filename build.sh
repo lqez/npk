b4f61413159e76c266cc811fbf16c244ea85be23
Park Hyunwoo <ez.amiryo@gmail.com>
2018-11-28 20:21:14 +0900

    Add multiple os into travis conf

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 mkdir "_build"
 cd "_build"
-cmake .. -DDEV_MODE=True
-make && make test
+if [ "$TRAVIS_OS_NAME" == "windows" ]; then
+    cmake .. -DCMAKE_SH="CMAKE_SH-NOTFOUND" -G "MinGW Makefiles" -DDEV_MODE=True
+    ls -l
+    mingw32-make
+else
+    cmake .. -DDEV_MODE=True
+    make && make test
+fi
